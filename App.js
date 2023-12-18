470afbd4a5a82c7fb475fe861a08a918b317e1c4
ShaheerShah079 <145196564+ShaheerShah079@users.noreply.github.com>
2023-12-19 00:04:53 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,170 +1,187 @@
-import React, { useEffect, useState } from 'react'
-import {View,TouchableOpacity,Text, FlatList} from 'react-native'
-import { TextInput } from 'react-native-paper';
-import SQLite from 'react-native-sqlite-storage'
+import React,{useEffect,useState} from 'react'
+import { View, Text ,TouchableOpacity,TextInput,StyleSheet} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SQLite from 'react-native-sqlite-storage';
 const db=SQLite.openDatabase(
   {
     name:'Record',
-    location:'default',
+    location:'default'
   },
-  ()=>{console.log("Database Connected")},
-  error=>{console.log(error)}
-);
+  ()=>{console.log("Database connected Successfully")},
+  (e)=>{console.log(e)}
 
+)
 const createTable=()=>{
   db.transaction(
     (tx)=>{
-    tx.executeSql(
-      "CREATE TABLE IF NOT EXISTS USERS (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,AGE INTEGER,ADDRESS TEXT)",
-      [],
-      ()=>{console.log("User Table Created")},
-      (error)=>{console.log(error)}
-    )
-  })
+      tx.executeSql(
+        "Create Table if not exists RECORDS (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,AGE INTEGER,ADDRESS TEXT)",
+        [],
+        ()=>{console.log("Table Created Successfully")},
+        (e)=>{console.log(e)}
+      )
+    }
+  )
 }
-
- 
-function HomeScreen({navigation}) {
+const insertRecord=()=>{
+db.transaction(
+  (tx)=>{
+    tx.executeSql(
+      "Insert into RECORDS (NAME,AGE,ADDRESS) values ('Sheri',22,'Lahore,Pakistan'),('Zain',25,'Sialkot,Pakistan'),"+
+      "('Sara',24,'Muzafarabad,Pakistan'),('Fazeela',22,'Lahore,Pakistan'),('Qandeel',15,'Lahore,Pakistan'),"+
+      "('Laiba',19,'Mansehra,Pakistan'),('Sami',18,'Abbottabad,Pakistan'),('Wasi',20,'Islamabad,Pakistan'),"+
+      "('Mamoona',21,'Abbottabad,Pakistan'),('Shumaila',28,'Karachi,Pakistan')",
+      [],
+      ()=>{console.log("Data inserted")},
+      (e)=>{console.log('error '+e)}
+      )
+  }
+)
+}
+function SearchRecord({navigation}) {
+  const[id,setId]=useState("")
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TouchableOpacity onPress={()=>createTable()}><Text>Create Table</Text></TouchableOpacity>
-<TouchableOpacity onPress={()=>navigation.navigate('Insert')}><Text>Insert Data</Text></TouchableOpacity>
-<TouchableOpacity onPress={()=>navigation.navigate('Update')}><Text>Update Data</Text></TouchableOpacity>
-<TouchableOpacity onPress={()=>navigation.navigate('View')}><Text>View Data</Text></TouchableOpacity>
-<TouchableOpacity onPress={()=>navigation.navigate('Delete')}><Text>Delete Data</Text></TouchableOpacity>
+
+<View style={{flexDirection:'row'}}>
+     <View style={{flexDirection:'column', alignItems:'center'}}>
+      <Text style={styles.txt}>Enter ID:      </Text>
+      </View>
+
+      <View style={{flexDirection:'column'}}>
+        <TextInput style={styles.txtIn} value={id} onChangeText={setId}></TextInput>
+       <Text>{"\n"}</Text>
+      <TouchableOpacity style={styles.btn} onPress={()=>{
+        navigation.navigate('View Record',{'id':id})
+        }}><Text style={styles.btntxt}>Search</Text></TouchableOpacity>
+    </View> 
+    </View> 
     </View>
   );
 }
-function ViewData() {
-  const[record,setRecord]=useState([])
-  useEffect(()=>{
-seeData()
-  },[])
-  const seeData=()=>{
+function ViewRecord({route}) {
+  id=route.params?.id
+  useEffect(
+    ()=>{
+    getData()
+    },[]
+  )
+  const[record,setRecord]=useState({})
+  const getData=()=>{
     db.transaction(
       (tx)=>{
         tx.executeSql(
-          "SELECT * FROM USERS",
-          [],
-          (z,results)=>{
-           var length=results.rows.length
-           var data=[]
-            if(length>0){
-              for(let i=0;i<length;i++){
-                data.push(results.rows.item(i))
-              }
-              setRecord(data)
+          "Select * from RECORDS where ID=?",
+          [id],
+           (z,result)=>{
+            var length=result.rows.length
+            if(length==1){
+              console.log(result.rows.item(0))
+              setRecord(result.rows.item(0))
             }
           },
-          (error)=>{console.log(error)}
+          (e)=>{console.log(e)}
         )
       }
     )
   }
-  return (
+    return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <FlatList data={record}
-      renderItem={({item})=>(<Text>{item.NAME}           {item.AGE}           {item.ADDRESS}</Text>)}/>
+    <View style={{flexDirection:'column', alignItems:'center'}}>
+    <View style={{flexDirection:'row', alignItems:'center'}}>
+      <Text style={styles.txt}>ID:                  </Text>
+      <Text style={styles.txtIn}>{record.ID}</Text>
+      </View>
+      </View>
+
+      <Text>{"\n"}</Text>
+      <View style={{flexDirection:'column', alignItems:'center'}}>
+    <View style={{flexDirection:'row', alignItems:'center'}}>
+      <Text style={styles.txt}>NAME:            </Text>
+      <Text style={styles.txtIn}>{record.NAME}</Text>
+      </View>
+      </View>
+
+      <Text>{"\n"}</Text>
+      <View style={{flexDirection:'column', alignItems:'center'}}>
+    <View style={{flexDirection:'row', alignItems:'center'}}>
+      <Text style={styles.txt}>AGE:               </Text>
+      <Text style={styles.txtIn}>{record.AGE}</Text>
+      </View>
+      </View>  
+    
+      <Text>{"\n"}</Text>
+      <View style={{flexDirection:'column'}}>
+    <View style={{flexDirection:'row'}}>
+      <Text style={styles.txt} >ADDRESS:      </Text>
+      <Text style={styles.txtInAdd} >{record.ADDRESS}</Text>
+      </View>
+      </View>
+
     </View>
   );
 }
 
-function InsertData() {
-  const[name,setName]=useState("")
-  const[age,setAge]=useState("")
-  const[address,setAddress]=useState("")
-  const setData=()=>{
-    db.transaction(
-     (tx)=>{
-       tx.executeSql(
-         "INSERT INTO USERS (NAME,AGE,ADDRESS) VALUES (?,?,?)",
-         [name,age,address],
-         ()=>{console.log("Record Inserted Successfully")},
-         (error)=>{console.log(error)}
-       )
-     }
-   )
- }
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-     <TextInput
-      label="Name" value={name} onChangeText={setName}/>
-      <Text>{"\n"}</Text>
-      <TextInput label="Age" value={age} onChangeText={setAge}/>
-      <Text>{"\n"}</Text>
-      <TextInput label="Address"  value={address} onChangeText={setAddress}/>
-      <Text>{"\n"}</Text>
-      <TouchableOpacity onPress={()=>{setData()}}><Text>Insert Data</Text></TouchableOpacity>
-    </View>
-  );
-}
 
-function UpdateData() {
-  const[id,setId]=useState("")
-  const[name,setName]=useState("")
-const changeData=()=> {
-   db.transaction(
-    (tx)=>{
-      tx.executeSql(
-        "UPDATE USERS SET NAME=? WHERE ID=?",
-        [name,id],
-        ()=>{console.log("Updated Sucessfully")},
-        (error)=>{console.log(error)}
-      )
-    }
-   )
-  }
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TextInput
-      label="id" value={id} onChangeText={setId}/>
-      <Text>{"\n"}</Text>
-      <TextInput
-      label="Name" value={name} onChangeText={setName}/>
-      <Text>{"\n"}</Text>
-<TouchableOpacity onPress={()=>changeData()}><Text>Update Data</Text></TouchableOpacity>
-    </View>
-  );
-}
-function DeleteData(){
-  const[id,setId]=useState("")
-function del(){
-  db.transaction(
-    (tx)=>{
-      tx.executeSql(
-        "DELETE FROM USERS WHERE ID=?",
-        [id],
-        ()=>{console.log("Deleted Sucessfully")},
-        (error)=>{console.log(error)}
-      )
-    }
-  )
-}
-  return(
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <TextInput
-    label="id" value={id} onChangeText={setId}/>
-    <Text>{"\n"}</Text>
-<TouchableOpacity onPress={()=>del()}><Text>Delete Data</Text></TouchableOpacity>
-  </View>
-  )
-}
 const Stack = createNativeStackNavigator();
 function App() {
+  // useEffect(
+  //   ()=>{
+  // createTable()
+  // insertRecord()
+  //   },[]
+  // )
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Insert" component={InsertData} />
-        <Stack.Screen name="Update" component={UpdateData} />
-        <Stack.Screen name="View" component={ViewData} />
-        <Stack.Screen name="Delete" component={DeleteData} />
+        <Stack.Screen name="Search Record" component={SearchRecord} />
+        <Stack.Screen name="View Record" component={ViewRecord} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
+const styles=StyleSheet.create(
+  {
+    txtIn:{
+      width:150,
+      height:40,
+      backgroundColor:'#d9d9d9',
+      borderRadius:4,
+      color:'black',
+      fontSize:18,
+      padding:8
+      
+    },
+    txtInAdd:{
+      width:150,
+      height:80,
+      backgroundColor:'#d9d9d9',
+      borderRadius:4,
+      color:'black',
+      fontSize:18,
+      textAlignVertical: 'top',
+      padding:8
+    },
+    btn:{
+      width:150,
+      height:40,
+      borderRadius:7,
+      backgroundColor:'#9ba0a8'
+      
+    },
+    btntxt:{
+      fontWeight:'bold',
+      color:'black',
+     margin:9,
+      alignSelf:'center'
+    },
+    txt:{
+      padding:7,
+      fontWeight:'bold',
+      fontSize:18
+    }
+  }
+)
